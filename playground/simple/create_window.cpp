a80ecb21a831e2e83b4b91ee37839c2cd66d9888
Jedzia <jed69@gmx.de>
2020-03-03 20:58:18 +0100

    added glbinding library

---
+++ playground/simple/create_window.cpp
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of OpenGLTemplate, R&D.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2020, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             the create_window.cpp class.
+ * \file       create_window.cpp
+ * \date       2020-01-23
+ * \author     Jedzia.
+ *
+ * modified    2020-01-23, Jedzia
+ */
+/*---------------------------------------------------------*/
+
+#include <iostream> // include API
+
+using namespace std;
+
+/** Brief description of $(fclass), main
+ *  Detailed description.
+ *  @return TODO
+ */
+int main(){ // the main code portion of a C++ program
+    cout << "Hello cheesy World" << endl;  //print Hello World on the screen
+    return 0; // conventional
+}
