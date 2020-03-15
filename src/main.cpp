6567a074d6e83ec579a8a9c4d968a80a7354dbf6
Jedzia <jed69@gmx.de>
2020-03-15 19:16:39 +0100

    lower case functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,54 @@
-//
-// Created by Jedzia on 14.03.2020.
-//
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of OpenGLTemplate, R&D.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2020, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             the main.cpp class.
+ * \file       main.cpp
+ * \date       2020-03-14
+ * \author     Jedzia.
+ *
+ * modified    2020-03-14, Jedzia
+ */
+/*---------------------------------------------------------*/
 
 #include "precompiled.h"
+//
+#include <boost/locale/info.hpp>
 #include <grcore/grcore.h>
 #include <grgraphics/grgraphics.h>
-#include <boost/locale/info.hpp>
 
 /*
-#include <SFML/Graphics.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/System/Thread.hpp>
-#include <fmt/format.h>
-#include <iostream>
-*/
+   #include <SFML/Graphics.hpp>
+   #include <SFML/System/Clock.hpp>
+   #include <SFML/System/Thread.hpp>
+   #include <fmt/format.h>
+   #include <iostream>
+ */
 
 const bool USE_VSYNC = false;
 
+/** Brief description of $(fclass), main
+ *  Detailed description.
+ *  @return TODO
+ */
 int main() {
     constexpr int WINDOW_WIDTH = 1280;
     constexpr auto WINDOW_RATIO = std::make_tuple(16.0, 9.0);
-    constexpr int WINDOW_HEIGHT = static_cast<int>( WINDOW_WIDTH * std::get<1>(WINDOW_RATIO) /
-                                                    std::get<0>(WINDOW_RATIO));
+    constexpr int WINDOW_HEIGHT = static_cast<int>(WINDOW_WIDTH * std::get<1>(WINDOW_RATIO) /
+                                                   std::get<0>(WINDOW_RATIO));
 
     core::bla();
     auto title = fmt::format("SFML Window ({0}, {1} -> {2:.1f}:{3})", WINDOW_WIDTH, WINDOW_HEIGHT,
             std::get<0>(WINDOW_RATIO), std::get<1>(WINDOW_RATIO));
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), title);
-    if(USE_VSYNC)
+    if(USE_VSYNC) {
         window.setVerticalSyncEnabled(true);
-    else
+    } else {
         window.setFramerateLimit(60 * 4);
+    }
 
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -45,6 +63,7 @@ int main() {
     double fps_time = 0;
 
     sf::Clock clock; // starts the clock
+
     while(window.isOpen()) {
         sf::Time elapsed = clock.restart();
         float elapsedSeconds = elapsed.asSeconds();
@@ -53,13 +72,16 @@ int main() {
         //clock.restart();
 
         sf::Event event{};
+
         while(window.pollEvent(event)) {
             /*switch(event.type)
                 case sf::Event::Closed:
                     window.close();*/
 
-            if(event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed) {
                 window.close();
+            }
+
             if(event.type == sf::Event::KeyPressed) {
                 if(event.key.code == sf::Keyboard::Escape) {
                     std::cout << "the escape key was pressed" << std::endl;
@@ -70,17 +92,14 @@ int main() {
                     window.close();
                 }
             }
-            if(event.type == sf::Event::KeyPressed) {
-            }
+
+            if(event.type == sf::Event::KeyPressed) {}
         }
-
-
         /*if(elapsedSeconds < 0.01)
-        {
+           {
             const char *formatStr = "FPS: {:.1f}, Frame Counter: {}";
             fpsDisplay.setString(fmt::format(formatStr, 1.0 / elapsedSeconds, frame_counter));
-        }*/
-
+           }*/
 
         if(frame_counter % 60 == 0) {
             const char *formatStr = "FPS: {:.1f}, Frame Counter: {}";
@@ -92,24 +111,27 @@ int main() {
         shape.setPosition(x, y);
         x += Speed * elapsedSeconds;
         y += Speed * 0.5f * elapsedSeconds;
-        if(x > 500)
+        if(x > 500) {
             x = 0;
-        if(y > 500)
+        }
+
+        if(y > 500) {
             y = 0;
+        }
 
         window.clear();
         window.draw(shape);
         window.draw(fpsDisplay);
         window.display();
 
-        if(USE_VSYNC)
+        if(USE_VSYNC) {
             sf::sleep(sf::milliseconds(12));
-        else
+        } else {
             sf::sleep(sf::milliseconds(1));
+        }
 
         //sf::Thread::wait(1);
         frame_counter++;
     }
-
     return 0;
-}
+} // main
