eec9c1ebd883742ea5b4fbce565e6233dbb8165a
Jedzia <jed69@gmx.de>
2020-03-15 16:06:09 +0100

    added graphics main library

---
+++ grgraphics/include/grgraphics/grgraphics.h
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of GrCore, the efficient I/O library.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2020, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             some items for initial R&D testing.
+ * \file       grcore.h
+ * \date       2020-01-23
+ * \author     Jedzia.
+ *
+ * modified    2020-01-23, Jedzia
+ */
+/*---------------------------------------------------------*/
+#ifndef GRCORE_GRCORE_H
+#define GRCORE_GRCORE_H
+
+namespace grg {
+void bla();
+}
+#endif //GRCORE_GRCORE_H
---
+++ grgraphics/src/test.cpp
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------*/
+/*!
+ * This file is part of GrCore, the efficient I/O library.
+ * License details can be found in the file COPYING.
+ * Copyright (c) 2020, EvePanix. All rights reserved.
+ *
+ * \brief      This file contains the definition of
+ *             the test.cpp class.
+ * \file       test.cpp
+ * \date       2020-01-23
+ * \author     Jedzia.
+ *
+ * modified    2020-01-23, Jedzia
+ */
+/*---------------------------------------------------------*/
+#include <grgraphics/grgraphics.h>
+//
+#include <iostream>
+
+namespace grg {
+/** Brief description of $(fclass), bla
+ *  Detailed description.
+ *
+ */
+void bla() {
+    int a = 0;
+    a++;
+    std::cout << "Hello from the Speed IO Department, test ... test ;)" << a << std::endl;
+}
+}
