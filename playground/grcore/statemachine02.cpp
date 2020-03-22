279d50a6dcf1a22ed65edc5b274fa98a87423544
Jedzia <jed69@gmx.de>
2020-03-22 18:12:28 +0100

    formatting

--- playground/grcore/statemachine02.cpp
+++ playground/grcore/statemachine02.cpp
@@ -14,10 +14,10 @@
  */
 /*---------------------------------------------------------*/
 
+#include <afsm/fsm.hpp>
 #include <fmt/printf.h>
 #include <grcore/wgl_common.h>
 #include <iostream>
-#include <afsm/fsm.hpp>
 //
 #include <winuser.h>
 
@@ -26,17 +26,15 @@ using namespace std;
 // see https://github.com/zmij/afsm/tree/c++17/examples
 // Events
 struct start {
-    template <typename Event, typename FSM>
-    void
-    operator()(Event const&, FSM& fsm) const
-    {
+    template<typename Event, typename FSM>
+    void operator()(Event const&, FSM& fsm) const {
         cout << "OPERATOR reload" << endl;
         root_machine(fsm).reload();
     }
 };
+
 struct stop {
-    void operator()() const
-    {
+    void operator()() const {
         cout << "OPERATOR stop" << endl;
     }
 };
@@ -46,70 +44,62 @@ void p(const std::string& msg) {
 }
 
 // Actions
-struct do_start{
-    template < typename Event, typename FSM, typename SourceState, typename TargetState >
-    void
-    operator()(Event const&, FSM& fsm, SourceState&, TargetState&) const
-    {
+struct do_start {
+    template<typename Event, typename FSM, typename SourceState, typename TargetState>
+    void operator()(Event const&, FSM& fsm, SourceState&, TargetState&) const {
         p("do start");
         root_machine(fsm).doSomething();
     }
 };
 struct do_stop {
-    template < typename Event, typename FSM, typename SourceState, typename TargetState >
-    void
-    operator()(Event const&, FSM&, SourceState&, TargetState&) const
-    {
+    template<typename Event, typename FSM, typename SourceState, typename TargetState>
+    void operator()(Event const&, FSM&, SourceState&, TargetState&) const {
         p("do stop");
     }
 };
 
-
 // State machine definition
-struct minimal_def : ::afsm::def::state_machine<minimal_def> {
+struct minimal_def : ::afsm::def::state_machine<minimal_def>{
     //@{
     /** @name States */
-    struct initial      : state<initial> {  };
-    struct running      : state<running> {
-        template < typename Event, typename FSM >
-        void
-        on_enter(Event&& /*evt*/, FSM& /*fsm*/)
-        {
+    struct initial : state<initial>{  };
+    struct running : state<running>{
+        template<typename Event, typename FSM>
+        void on_enter(Event&& /*evt*/, FSM& /*fsm*/) {
             ::std::cout << "state<running> enter" << ::std::endl;
         }
-        template < typename Event, typename FSM >
-        void
-        on_exit(Event const& /*evt*/, FSM& /*fsm*/)
-        {
+
+        template<typename Event, typename FSM>
+        void on_exit(Event const& /*evt*/, FSM& /*fsm*/) {
             ::std::cout << "state<running> exit" << ::std::endl;
         }
     };
-    struct terminated   : terminal_state<terminated> {};
+    struct terminated : terminal_state<terminated>{};
     //@}
-
-    using initial_state = initial;
-    using transitions   = transition_table<
-    // *INDENT-OFF*
-    /*  State       Event       Next          Action  */
-    tr< initial,    start,      running,     do_start     >,
-    tr< running,    stop,       terminated,  do_stop      >
-    >;
-    // *INDENT-ON*
 
     static void doSomething() {
         cout << "doSomething()" << endl;
     }
+
+    using initial_state = initial;
+    using transitions = transition_table <
+// *INDENT-OFF*
+    /*  State       Event       Next          Action  */
+    tr< initial,    start,      running,     do_start     >,
+    tr< running,    stop,       terminated,  do_stop      >
+    >;
+
+// *INDENT-ON*
 };
 
 // State machine object
 using minimal = ::afsm::state_machine<minimal_def>;
 
-void use()
-{
+void use(){
     //int x = 5;
-    minimal fsm{};
-    fsm.process_event(start{});
-    fsm.process_event(stop{});
+    minimal fsm {};
+    fsm.process_event(start {});
+    fsm.process_event(stop {});
 }
 
 /** Program Entry Function, main
