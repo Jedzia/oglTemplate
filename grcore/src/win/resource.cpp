0b96a1be7b4a36419007ac6674335b824c9e0267
Jedzia <jed69@gmx.de>
2020-03-26 11:59:54 +0100

    tidy up

--- grcore/src/win/resource.cpp
+++ grcore/src/win/resource.cpp
@@ -10,21 +10,15 @@
  * \date       2020-03-25
  * \author     Jedzia.
  *
- * modified    2020-03-25, Jedzia
+ * modified    2020-03-26, Jedzia
  */
 /*---------------------------------------------------------*/
-//
-// Created by Jedzia on 26.03.2020.
-//
 
-//#include <libloaderapi.h>
-//#include <winuser.h>
 #include "grcore/resource.h"
 #include <iostream>
 #include <stdexcept>
 #include <windows.h>
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wunused-variable"
+
 core::Resource::Resource(int resourceName) : m_id(resourceName) {}
 
 core::Resource::~Resource() {
@@ -36,10 +30,6 @@ std::tuple<const void *, size_t, int> core::Resource::Get() {
     static_cast<void>(m_id);
     //int type = 333; //static_cast<int>(RT_FONT);
     int error = 0;
-    //int name = 129;
-    //int type = 256;
-    //int name = 255;
-    //HMODULE handle = ::GetModuleHandleW(NULL);
     if(!p_handle) {
         p_handle = ::GetModuleHandleW(L"libGrCore.dll");
         std::cout << __PRETTY_FUNCTION__ << ": Handle via GetModuleHandleW(L\"libGrCore.dll\")" << std::endl;
@@ -50,23 +40,15 @@ std::tuple<const void *, size_t, int> core::Resource::Get() {
     }
 
     HMODULE handle = static_cast<HMODULE>(p_handle);
-    //HRSRC rc = ::FindResourceW(handle, MAKEINTRESOURCEW(name), MAKEINTRESOURCEW(type));
     HRSRC rc = ::FindResourceW(handle, MAKEINTRESOURCEW(m_id), L"BINARY");
-    //HRSRC rc = ::FindResource(handle, MAKEINTRESOURCE(name), MAKEINTRESOURCE(type));
     HGLOBAL rcData = ::LoadResource(handle, rc);
     auto size = ::SizeofResource(handle, rc);
-    //static_cast<void>(size);
-    //auto data = static_cast<const wchar_t*>(::LockResource(rcData));
-    //auto data = static_cast<const char *>(::LockResource(rcData));
     auto data = static_cast<const void *>(::LockResource(rcData));
-    //static_cast<void>(data);
+    //LockResource does not actually lock memory; it is just used to obtain a pointer to the memory
+    // containing the resource data.
     return {
                data, size, error
     };
-    //LockResource does not actually lock memory; it is just used to obtain a pointer to the memory
-    // containing the resource data.
 } // core::Resource::Get
 
 void * core::Resource::p_handle = nullptr;
-
-//#pragma clang diagnostic pop
