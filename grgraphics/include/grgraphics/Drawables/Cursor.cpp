eeb454f7e7851e5a19a6c7d4ea8f23884970d45e
Jedzia <jed69@gmx.de>
2020-03-28 11:53:47 +0100

    added a cursor drawable

--- grgraphics/include/grgraphics/Drawables/Cursor.cpp
+++ grgraphics/include/grgraphics/Drawables/Cursor.cpp
@@ -5,11 +5,13 @@
  * Copyright (c) 2020, EvePanix. All rights reserved.
  *
  * \brief      This file contains the definition of
- *             the $(filename) class.
- * \file       $(filename)
+ *             the Cursor.cpp class.
+ * \file       Cursor.cpp
  * \date       2020-03-28
  * \author     Jedzia.
  *
  * modified    2020-03-28, Jedzia
  */
 /*---------------------------------------------------------*/
+
+#include "Cursor.h"
--- grgraphics/include/grgraphics/Drawables/Cursor.h
+++ grgraphics/include/grgraphics/Drawables/Cursor.h
@@ -5,11 +5,19 @@
  * Copyright (c) 2020, EvePanix. All rights reserved.
  *
  * \brief      This file contains the definition of
- *             the $(filename) class.
- * \file       $(filename)
+ *             the Cursor.h class.
+ * \file       Cursor.h
  * \date       2020-03-28
  * \author     Jedzia.
  *
  * modified    2020-03-28, Jedzia
  */
 /*---------------------------------------------------------*/
+
+#ifndef OPENGLTEMPLATE_ED62CB437F5146F2ADDE61CC764404BF_CURSOR_H
+#define OPENGLTEMPLATE_ED62CB437F5146F2ADDE61CC764404BF_CURSOR_H
+
+namespace grg {
+class Cursor {};
+}
+#endif//OPENGLTEMPLATE_ED62CB437F5146F2ADDE61CC764404BF_CURSOR_H
