9098f2867811f99e3198a73df6b0a814a6d38e9a
Jedzia <jed69@gmx.de>
2020-04-05 13:18:20 +0200

    API documentation

--- grcore/include/grcore/Math.h
+++ grcore/include/grcore/Math.h
@@ -10,7 +10,7 @@
  * \date       2020-03-23
  * \author     Jedzia.
  *
- * modified    2020-03-23, Jedzia
+ * modified    2020-04-05, Jedzia
  */
 /*---------------------------------------------------------*/
 
@@ -20,34 +20,65 @@
 #include <tuple>
 
 namespace grcore {
+/** @class WindowCalculator:
+ *  Calculates window dimensions by ratio.
+ */
 class WindowCalculator {
 public:
 
+    /**
+     * @brief Creates a new instance by width and ratio.
+     *
+     * The height is calculated by width and ratio.
+     * @param width The horizontal width of the window.
+     * @param ratioAntecedent Left side of the ratio calculation, as 16 in 16:9.
+     * @param ratioConsequent Right side of the ratio calculation, as 9 in 16:9.
+     */
     explicit WindowCalculator(unsigned int width, double ratioAntecedent, double ratioConsequent);
     // explicit WindowCalculator(double ratio_antecedent, double ratio_consequent, int height);
 
+    /** @brief Calculate the height by width and ratio.
+     *
+     * The height is calculated by width and ratio.
+     * @param width The horizontal width of the window.
+     * @param ratioAntecedent Left side of the ratio calculation, as 16 in 16:9.
+     * @param ratioConsequent Right side of the ratio calculation, as 9 in 16:9.
+     *  @return the height resulting from the calculation of the parameters.
+     */
     static double CalcHeight(unsigned int width, double ratioAntecedent, double ratioConsequent);
 
-    //double calcWidth(int height, double ratio_antecedent, double ratio_consequent);
-    //
+    /** @brief Get the window width.
+     *  @return the width of the window.
+     */
     [[nodiscard]] unsigned int GetWidth() const;
 
+    /** @brief Get the window height.
+     *  @return the height of the window.
+     */
     [[nodiscard]] unsigned int GetHeight() const;
 
+    /** @brief Get the window ratio.
+     *  @return the left hand side of the ratio of the window, like 16 in 16:9.
+     */
     [[nodiscard]] double GetRatioAntecedent() const;
 
+    /** @brief Get the window ratio.
+     *  @return the right hand side of the ratio of the window, like 9 in 16:9.
+     */
     [[nodiscard]] double GetRatioConsequent() const;
 
+    /** @brief Get the window ratio as tuple.
+     *  @return the ratio of the window, tuple(16, 9) for a 16:9 ratio.
+     */
     [[nodiscard]] std::tuple<double, double> GetRatio() const;
 
 private:
 
     unsigned int m_width = 0;
     unsigned int m_height = 0;
-    //std::tuple<double, double> m_ratio;
     double m_ratioAntecedent = 0;
     double m_ratioConsequent = 0;
 };
-}// namespace core
+}// namespace grcore
 
 #endif//OPENGLTEMPLATE_07CF1308072042EB975B05372D54C611_MATH_H
--- grcore/src/Math.cpp
+++ grcore/src/Math.cpp
@@ -10,37 +10,36 @@
  * \date       2020-03-23
  * \author     Jedzia.
  *
- * modified    2020-03-23, Jedzia
+ * modified    2020-04-05, Jedzia
  */
-/*---------------------------------------------------------*/
-//
-// Created by Jedzia on 23.03.2020.
-//
 
 #include "grcore/Math.h"
+
 grcore::WindowCalculator::WindowCalculator(unsigned int width, double ratioAntecedent, double ratioConsequent) : m_width(width),
     m_ratioAntecedent(ratioAntecedent), m_ratioConsequent(ratioConsequent) {
     m_height = static_cast<unsigned int>(CalcHeight(width, ratioAntecedent, ratioConsequent));
 }
 
-/*core::WindowCalculator::WindowCalculator(double ratioAntecedent, double ratioConsequent, int height) : m_height(height),
-    m_ratioAntecedent(ratioAntecedent), m_ratioConsequent(ratioConsequent) {}*/
-
 double grcore::WindowCalculator::CalcHeight(unsigned int width, double ratioAntecedent, double ratioConsequent) {
     return width * ratioConsequent / ratioAntecedent;
 }
+
 unsigned int grcore::WindowCalculator::GetWidth() const {
     return m_width;
 }
+
 unsigned int grcore::WindowCalculator::GetHeight() const {
     return m_height;
 }
+
 double grcore::WindowCalculator::GetRatioAntecedent() const {
     return m_ratioAntecedent;
 }
+
 double grcore::WindowCalculator::GetRatioConsequent() const {
     return m_ratioConsequent;
 }
+
 std::tuple<double, double> grcore::WindowCalculator::GetRatio() const {
     return std::make_tuple(m_ratioAntecedent, m_ratioConsequent);
 }
