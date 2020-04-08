250e787283e9f3b494604493988e7a71c2c6fb2a
jedzia <jed69@gmx.de>
2020-04-08 09:02:52 +0000

    resource header. should be generated, later

---
+++ grcore/src/linux/GrCore.rc.s.Resource.h
@@ -0,0 +1,23 @@
+//
+// Created by jedzia on 4/8/20.
+//
+
+#ifndef OPENGLTEMPLATE_RESOURCE_H
+#define OPENGLTEMPLATE_RESOURCE_H
+
+//namespace grcore {
+//    namespace res {
+
+#define IDR_MYFONT  129
+#define IDR_DATA1  255
+
+
+extern char IDR_MYFONT_D[];
+extern unsigned IDR_MYFONT_D_size;
+extern char IDR_DATA1_D[];
+extern unsigned IDR_DATA1_D_size;
+
+//    }
+//}
+
+#endif //OPENGLTEMPLATE_RESOURCE_H
--- grcore/src/linux/Resource.cpp
+++ grcore/src/linux/Resource.cpp
@@ -16,80 +16,42 @@
 
 #include "grcore/Resource.h"
 #include "grcore/warning/FMT_format_log.h"
+#include "GrCore.rc.s.Resource.h"
 #include <iostream>
 #include <stdexcept>
-//#include <windows.h>
 
-//extern void* G_HINST_DLL;
-void *G_HINST_DLL = 0;
-
-extern char IDR_MYFONT[];
-extern unsigned IDR_MYFONT_size;
-extern char IDR_DATA1[];
-extern unsigned IDR_DATA1_size;
 
 grcore::Resource::Resource(int resourceName) : m_id(resourceName) {
-    //std::cout << "+++ Constructor " << __PRETTY_FUNCTION__ << " called. +++" << "\n";
     spdlog::debug(" {1}:{2} +++ Constructor [{0}] (resourceName={0}) called. +++", __PRETTY_FUNCTION__, __FILE__,
                   __LINE__, resourceName);
 }
 
 grcore::Resource::~Resource() {
-    //std::cout << "+++ Destructor  " << __PRETTY_FUNCTION__ << " called. +++" << "\n";
     spdlog::debug(" {1}:{2} +++ Destructor  [{0}]  called. +++", __PRETTY_FUNCTION__, __FILE__, __LINE__);
     // no need to free something, its a embedded resource.
-    //if(p_handle)
 }
 
 std::tuple<const void *, size_t, int> grcore::Resource::Get() {
-    static_cast<void>(m_id);
-    //int type = 333; //static_cast<int>(RT_FONT);
     int error = 0;
-    /*if(!G_HINST_DLL) {
-        //G_HINST_DLL = ::GetModuleHandleW(L"libGrCore.dll");
-        std::cout << __PRETTY_FUNCTION__ << ": Handle via GetModuleHandleW(L\"libGrCore.dll\")" << "\n";
-    }
-
-    if(!G_HINST_DLL) {
-        throw std::runtime_error("Missing DLL Handle");
-    }*/
-
     auto size = 0;
     //auto data = static_cast<const void *>(::LockResource(rcData));
     const void *data = nullptr;
 
+    //using namespace res;
     switch (m_id) {
-        case 129:
-            data = static_cast<const void *>(IDR_MYFONT);
-            size = static_cast<int>(IDR_MYFONT_size);
+        case IDR_MYFONT:
+            data = static_cast<const void *>(IDR_MYFONT_D);
+            size = static_cast<int>(IDR_MYFONT_D_size);
             break;
-        case 255:
-            data = static_cast<const void *>(IDR_MYFONT);
-            size = static_cast<int>(IDR_MYFONT_size);
+        case IDR_DATA1:
+            data = static_cast<const void *>(IDR_DATA1_D);
+            size = static_cast<int>(IDR_DATA1_D_size);
             break;
         default:
             throw std::runtime_error(
                     fmt::format("[{}]: unhandled default case with resource id={}", __PRETTY_FUNCTION__, m_id));
     }
 
-    /*if (m_id == 129) {
-        data = static_cast<const void *>(IDR_MYFONT);
-        size = static_cast<int>(IDR_MYFONT_size);
-    } else if (m_id == 255) {
-        data = static_cast<const void *>(IDR_MYFONT);
-        size = static_cast<int>(IDR_MYFONT_size);
-    }*/
-
-
-    //HMODULE handle = static_cast<HMODULE>(G_HINST_DLL);
-    //HRSRC rc = ::FindResourceW(handle, MAKEINTRESOURCEW(m_id), L"BINARY");
-    //HGLOBAL rcData = ::LoadResource(handle, rc);
-    //auto size = ::SizeofResource(handle, rc);
-    //auto size = 12345;
-    //auto data = static_cast<const void *>(::LockResource(rcData));
-    //auto data = static_cast<const void *>("Foo-Bar");
-    //LockResource does not actually lock memory; it is just used to obtain a pointer to the memory
-    // containing the resource data.
     return {
             data, size, error
     };
