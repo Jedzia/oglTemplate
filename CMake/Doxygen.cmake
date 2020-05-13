aa2cf27c08536bd9d484e4b4eda6e00c89fb211a
Jedzia <jed69@gmx.de>
2020-05-13 19:11:15 +0200

    merged from cpp_starter_project, CMake modules

--- CMake/Doxygen.cmake
+++ CMake/Doxygen.cmake
@@ -1,11 +1,17 @@
 function(enable_doxygen)
   option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" OFF)
   if(ENABLE_DOXYGEN)
-    set(DOXYGEN_CALLER_GRAPH YES)
-    set(DOXYGEN_CALL_GRAPH YES)
-    set(DOXYGEN_EXTRACT_ALL YES)
-    #find_package(Doxygen REQUIRED dot)
-    find_package(Doxygen REQUIRED)
+    set(DOXYGEN_CALLER_GRAPH ON)
+    set(DOXYGEN_CALL_GRAPH ON)
+    set(DOXYGEN_EXTRACT_ALL ON)
+
+    # Refer to *build-dir*/CMakeDoxyfile.in and *build-dir*/Doxyfile.doxygen-docs for a list of
+    # available options.
+    set(DOXYGEN_EXCLUDE_PATTERNS */tmp/*)
+    set(DOXYGEN_USE_MDFILE_AS_MAINPAGE Readme.md)
+
+    find_package(Doxygen REQUIRED dot)
     doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
+
   endif()
 endfunction()
