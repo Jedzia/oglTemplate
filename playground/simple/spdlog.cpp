65b7c9b8fb2e5d051367ca2fc8e231fd0272b8a0
Jedzia <jed69@gmx.de>
2020-05-15 15:49:31 +0200

    added a spdlog play-around project

--- playground/simple/spdlog.cpp
+++ playground/simple/spdlog.cpp
@@ -5,11 +5,24 @@
  * Copyright (c) 2020, EvePanix. All rights reserved.
  *
  * \brief      This file contains the definition of
- *             the $(filename) class.
- * \file       $(filename)
+ *             the spdlog.cpp class.
+ * \file       spdlog.cpp
  * \date       2020-05-15
  * \author     Jedzia.
  *
  * modified    2020-05-15, Jedzia
  */
 /*---------------------------------------------------------*/
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+/** Program Entry Function, main
+ *  The designated start of the program.
+ *  @return program exit code.
+ */
+int main() {
+    // print Hello World on the screen
+    std::cout << "Hello cheesy World" << std::endl;
+    spdlog::info("Hello, {}!", "World");
+    return 0; // program exit code
+}
